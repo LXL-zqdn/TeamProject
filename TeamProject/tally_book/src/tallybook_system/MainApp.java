93f77b0d6704aacba3dc2b6095c35bb9fc7a82c3
maktine <90028270+maktine@users.noreply.github.com>
2021-11-25 22:10:24 +0800

    Add files via upload
    账目查询

---
+++ TeamProject/tally_book/src/tallybook_system/MainApp.java
@@ -0,0 +1,213 @@
+package tallybook_system;
+
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import tallybook_system.controller.LogupFrameController;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import java.sql.*;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    @FXML
+    private Stage primaryStage;
+
+    @FXML
+    private HBox rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("简易记账管理系统");
+        initLogupFrame();
+    }
+
+
+    /**
+         * 操作结果：登录界面
+         */
+    private Scene initLogupFrame() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("view/logupFrame.fxml"));
+            rootLayout = (HBox) loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+
+            LogupFrameController controller = loader.getController();
+            controller.setLogupStage(primaryStage);
+
+            primaryStage.show();
+            return scene;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /**
+     * 操作结果：主操作界面
+     */
+    public Scene initMainFrame() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/mainPage.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            Stage mainFrameStage = new Stage();
+            mainFrameStage.setTitle("管家婆系统");
+            mainFrameStage.setResizable(true);
+            mainFrameStage.setAlwaysOnTop(false);
+            mainFrameStage.initModality(Modality.APPLICATION_MODAL);
+            mainFrameStage.initOwner(primaryStage);
+
+            Scene scene = new Scene(page);
+            mainFrameStage.setScene(scene);
+            // 加载CSS样式文件
+           // scene.getStylesheets().add(MainApp.class.getResource(getStyleValue()).toExternalForm());
+
+            mainFrameStage.showAndWait();
+            return scene;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 操作结果：“报告”查询结果界面
+     */
+    public Scene initReportFrame() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/reportFrame.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            Stage mainFrameStage = new Stage();
+            mainFrameStage.setTitle("报告");
+            mainFrameStage.setResizable(true);
+            mainFrameStage.setAlwaysOnTop(false);
+            mainFrameStage.initModality(Modality.APPLICATION_MODAL);
+            mainFrameStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            mainFrameStage.setScene(scene);
+            // 加载CSS样式文件
+            //scene.getStylesheets().add(MainApp.class.getResource(getStyleValue()).toExternalForm());
+
+            mainFrameStage.showAndWait();
+            return scene;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /**
+     * 操作结果：查看数据表格界面
+     */
+    public Scene initTableView() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/tableView.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            Stage mainFrameStage = new Stage();
+            mainFrameStage.setTitle("查询结果");
+            mainFrameStage.setResizable(true);
+            mainFrameStage.setAlwaysOnTop(false);
+            mainFrameStage.initModality(Modality.APPLICATION_MODAL);
+            mainFrameStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            mainFrameStage.setScene(scene);
+
+            mainFrameStage.showAndWait();
+            return scene;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /**
+     * 操作结果：”按日期查询“结果界面
+     */
+    public Scene initDateCheckTableView() {
+        try {
+
+            Parent page = FXMLLoader.load(getClass().getResource("view/dateCheckFrame.fxml"));
+
+            Stage mainFrameStage = new Stage();
+            mainFrameStage.setTitle("按日期查询");
+            mainFrameStage.setResizable(true);
+            mainFrameStage.setAlwaysOnTop(false);
+            mainFrameStage.initModality(Modality.APPLICATION_MODAL);
+            mainFrameStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            mainFrameStage.setScene(scene);
+
+            mainFrameStage.showAndWait();
+            return scene;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /**
+     * 操作结果：”按分类查询“结果界面
+     */
+    public Scene initClassificationTableView() {
+        try {
+            Parent page = FXMLLoader.load(getClass().getResource("view/classificationCheckFrame.fxml"));
+
+            Stage mainFrameStage = new Stage();
+            mainFrameStage.setTitle("按分类查询");
+            mainFrameStage.setResizable(true);
+            mainFrameStage.setAlwaysOnTop(false);
+            mainFrameStage.initModality(Modality.APPLICATION_MODAL);
+            mainFrameStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            mainFrameStage.setScene(scene);
+
+            mainFrameStage.showAndWait();
+            return scene;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /**
+     * 操作结果：”按备注查询“结果界面
+     */
+    public Scene initMemoTableView() {
+        try {
+            Parent page = FXMLLoader.load(getClass().getResource("view/memoCheckFrame.fxml"));
+
+            Stage mainFrameStage = new Stage();
+            mainFrameStage.setTitle("按备注查询");
+            mainFrameStage.setResizable(true);
+            mainFrameStage.setAlwaysOnTop(false);
+            mainFrameStage.initModality(Modality.APPLICATION_MODAL);
+            mainFrameStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            mainFrameStage.setScene(scene);
+
+            mainFrameStage.showAndWait();
+            return scene;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
+
+
---
+++ TeamProject/tally_book/src/tallybook_system/controller/MainPageController.java
@@ -0,0 +1,430 @@
+package tallybook_system.controller;
+
+import tallybook_system.MainApp;
+import tallybook_system.bean.Session;
+import tallybook_system.bean.TableData;
+import tallybook_system.bean.User;
+import tallybook_system.dao.RecordDao;
+import tallybook_system.dao.UserDao;
+import tallybook_system.tools.DateTools;
+import tallybook_system.tools.PublicTools;
+import tallybook_system.tools.SimpleTools;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Properties;
+
+/**
+ * 主界面控制器
+ *
+ * @author lck100
+ */
+public class MainPageController {
+
+    private SimpleTools simpleTools = new SimpleTools();
+    private RecordDao recordDao = new RecordDao();
+    private MainApp mainApp = new MainApp();
+    private PublicTools publicTools = new PublicTools();
+    private DateTools dateTools = new DateTools();
+
+    @FXML
+    public RadioMenuItem defaultRadioMenuItem;
+
+    @FXML
+    public RadioMenuItem blackRadioMenuItem;
+
+    @FXML
+    public RadioMenuItem whiteRadioMenuItem;
+
+    @FXML
+    private MenuItem refreshContextMenu;
+
+    @FXML
+    private TableColumn<TableData, String> classificationColumn;
+
+    @FXML
+    private TextField totalOutputTextField;
+
+    @FXML
+    private TextField totalInputTextField;
+
+    @FXML
+    private ImageView userImage;
+
+    @FXML
+    private Label userNameLabel;
+
+    @FXML
+    private TableColumn<TableData, String> moneyColumn;
+
+    public TableView<TableData> getTableView() {
+        return tableView;
+    }
+
+    public void setTableView(TableView<TableData> tableView) {
+        this.tableView = tableView;
+    }
+
+    @FXML
+    private TableView<TableData> tableView;
+
+    @FXML
+    private TableColumn<TableData, String> typeColumn;
+
+    @FXML
+    private TableColumn<TableData, String> memoColumn;
+
+    @FXML
+    private TextField balanceTextField;
+
+    @FXML
+    private TableColumn<TableData, String> dateColumn;
+
+    @FXML
+    private TableColumn<TableData, String> idColumn;
+
+    /**
+     * 初始化界面信息
+     */
+    @FXML
+    public void initialize() {
+        // 刷新主键编号
+        new RecordDao().refreshPrimaryKey();
+        // 初始化用户记录
+        initUserRecord();
+        // 初始化表格数据
+        initAddDataToTableView();
+        // 初始化程序主题皮肤选择
+       // initThemeRadioMenuItem();
+    }
+
+    /**
+     * “导入”菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void importMenuItemEvent(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * “导出”菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void exportMenuItemEvent(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * “备份”菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void backupMenuItemEvent(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * “恢复”菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void recoverMenuItemEvent(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * “退出”菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void exitMenuItemEvent(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * ”添加“菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void addMenuItemEvent(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * ”删除“菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void deleteMenuItemEvent(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * ”修改“菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void alterMenuItemEvent(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * “删除”右键菜单的事件监听器
+     *
+     * @param event 事件
+     */
+    @FXML
+    public void deleteContextMenuEvent(ActionEvent event) {
+
+    }
+
+    /**
+     * “添加”右键菜单的事件监听器
+     *
+     * @param event 事件
+     */
+    @FXML
+    public void addContextMenuEvent(ActionEvent event) {
+
+    }
+
+    /**
+     * “修改”右键菜单的事件监听器
+     *
+     * @param event 事件
+     */
+    @FXML
+    public void alterContextMenuEvent(ActionEvent event) {
+
+    }
+
+    /**
+     * ”刷新“右键菜单的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    public void refreshContextMenuEvent(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * ”查询“菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void checkMenuItemEvent(ActionEvent actionEvent) {
+        // 打开查询界面
+        mainApp.initTableView();
+        initialize();
+
+
+    }
+
+    /**
+     * ”条形图“菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void barChartMenuItemEvent(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * “折线图”菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void lineChartMenuItemEvent(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * ”饼图“菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void pieChartMenuItemEvent(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * ”添加分类“菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void addClassificationMenuItemEvent(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * “用户信息”菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void userInfoMenuItemEvent(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * “报告”菜单项的事件监听器
+     *
+     * @param event 事件
+     */
+    @FXML
+    public void reportMenuItemEvent(ActionEvent event) {
+        // 打开报告界面
+        mainApp.initReportFrame();
+    }
+
+    /**
+     * “关于软件”菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void abutSoftMenuItemEvent(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * “帮助”菜单项的事件监听器
+     *
+     * @param actionEvent 事件
+     */
+    @FXML
+    public void helpMenuItemEvent(ActionEvent actionEvent) {
+
+    }
+    /**
+     * 操作结果：初始化用户名、总支出、总收入及余额
+     */
+    public void initUserRecord() {
+        // 获取登录成功的用户
+        User user = Session.getUser();
+        // 获取用户支出的总金额
+        float totalOutput = recordDao.getTotalAccount("支出", user.getUserId());
+        // 获取用户收入的总金额
+        float totalInput = recordDao.getTotalAccount("收入", user.getUserId());
+        // 获取余额
+        float balance = totalInput - totalOutput;
+        // 设置图片
+        userImage.setImage(new Image("file:" + new UserDao().selectUserById(Session.getUser().getUserId()).getUserImagePath()));
+        userImage.setSmooth(true);
+        userImage.setFitWidth(100);
+        userImage.setFitHeight(100);
+        userImage.setCache(false);
+        userImage.setPreserveRatio(false);
+        // 将支出金额、收入金额、余额、用户名设置到文本框种
+        totalOutputTextField.setText(String.valueOf(totalOutput));
+        totalInputTextField.setText(String.valueOf(totalInput));
+        balanceTextField.setText(String.valueOf(balance));
+        userNameLabel.setText(Session.getUser().getUserName());
+    }
+
+    /**
+     * 操作结果：初始化数据表视图
+     */
+    public void initAddDataToTableView() {
+        String sql = "select * from tb_records where uId=" + Session.getUser().getUserId() + ";";
+        publicTools.public_initTableViewData(tableView
+                , publicTools.public_getTableViewData(sql)
+                , idColumn
+                , typeColumn
+                , moneyColumn
+                , classificationColumn
+                , memoColumn
+                , dateColumn);
+    }
+    /**
+     * ”按日期查询“菜单项的事件监听器
+     *
+     * @param event 事件
+     */
+    @FXML
+    public void dateCheckMenuItemEvent(ActionEvent event) {
+        /**
+         * ”按日期查询“菜单项的事件监听器
+         *
+         * @param event 事件
+         */
+            // 打开按日期查询界面
+            mainApp.initDateCheckTableView();
+
+
+    }
+
+    /**
+     * ”按分类查询“菜单项的事件监听器
+     *
+     * @param event 事件
+     */
+    @FXML
+    public void classificationCheckMenuItemEvent(ActionEvent event) {
+        // 打开按分类查询界面
+        mainApp.initClassificationTableView();
+
+
+    }
+
+    /**
+     * ”按备注查询“菜单项的事件监听器
+     *
+     * @param event 事件
+     */
+    @FXML
+    public void memoCheckMenuItemEvent(ActionEvent event) {
+        // 打开按备注查询界面
+        mainApp.initMemoTableView();
+
+    }
+
+    /**
+     * “默认”菜单项的事件监听器方法
+     *
+     * @param event 事件
+     */
+    public void defaultRadioMenuItemEvent(ActionEvent event) {
+
+    }
+
+    /**
+     * “经典黑”菜单项的事件监听器方法
+     *
+     * @param event 事件
+     */
+    public void blackRadioMenuItemEvent(ActionEvent event) {
+
+    }
+
+    /**
+     * “优雅白”菜单项的事件监听器方法
+     *
+     * @param event 事件
+     */
+    public void whiteRadioMenuItemEvent(ActionEvent event) {
+
+    }
+}
